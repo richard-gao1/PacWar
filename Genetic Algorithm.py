7c05c38d3a931395fd372606db1efaf257c32681
Richard Gao <richard.g2002@gmail.com>
2024-01-25 17:58:52 -0600

    started writing the genetic algorithm (on the scoring)

--- Genetic Algorithm.py
+++ Genetic Algorithm.py
@@ -17,8 +17,43 @@ def generate_genes(num_genes: int) -> list:
         generated.append(gene)
     
     return generated
+
+# simulates round robin competition between genes in list
+# returns percentage of wins for each placement
+def round_robin(genes: list[int]) -> list[float]:
+    results = [0] * len(genes)
+    for i in range(len(genes) - 1):
+        for j in range(i+1, len(genes)):
+            (rounds, c1, c2) = _PyPacwar.battle(genes[i], genes[j])
+            score = score_simulation(rounds, c1, c2)
+            
+            
+    results = []
+
+# scores a simulation - proportional to 100 - 10:10 --> .5, 20-0 --> 1
+def score_simulation(rounds, c1, c2) -> float:
+    score = 0 # higher score is better
+    # checking if the simulation ended in a population completely winning
+    if (rounds < 500):
+        # checks if c1 or c2 wins
+        c1_wins = 1 if c1 > c2 else -1
+        if (rounds >= 300):
+            score = 17/20
+        elif(rounds >= 200):
+            score = 18/20
+        elif(rounds >= 100):
+            score = 19/20
+        else:
+            score = 1
+        score *= c1_wins
+    else: 
+        
+
+
+    return score
     
 # Example Python module in C for Pacwar
+# https://www.geeksforgeeks.org/genetic-algorithms/
 def main():
 
     num_genes = 50
