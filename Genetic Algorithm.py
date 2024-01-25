89338c71ed642dde264e4ef573ee8ebb6e07faed
Richard Gao <richard.g2002@gmail.com>
2024-01-25 17:35:46 -0600

    created generate_genes
    Co-authored-by: anyaguuu <ag156@rice.edu>

--- Genetic Algorithm.py
+++ Genetic Algorithm.py
@@ -1,9 +1,28 @@
 import _PyPacwar
 import numpy
+import random
 
+GENES = [0,1,2,3]
 
+# intially generates num_genes randomly
+def generate_genes(num_genes: int) -> list:
+    generated = []
+    for _ in range(num_genes):
+        gene = ""
+
+        for _ in range(50):
+            r = random.randrange(3)
+            gene += str(GENES[r])
+        
+        generated.append(gene)
+    
+    return generated
+    
 # Example Python module in C for Pacwar
 def main():
+
+    num_genes = 50
+    gene_list = generate_genes(num_genes)
 
     ones = [1] * 50
     threes = [3] * 50
