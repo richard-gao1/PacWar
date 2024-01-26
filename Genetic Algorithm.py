18d4f2ace7ae56fe4bd4924e7fe321c9e0cfa5e4
Richard Gao <richard.g2002@gmail.com>
2024-01-26 13:53:05 -0600

    put gene related operations into a gene class

--- Genetic Algorithm.py
+++ Genetic Algorithm.py
@@ -3,43 +3,101 @@ import argparse
 import numpy
 import random
 
+# valid genes
 GENES = [0,1,2,3]
+# length of a gene
+GENE_LENGTH = 50
+# population size
+POPULATION_SIZE = 100
 
-# intially generates num_genes randomly
-def generate_genes(num_genes: int) -> list:
-    generated = []
-    for _ in range(num_genes):
+class Gene():
+    def __init__(self, genome: list[str] = None):
+        """Gene Class initializer
+
+        :param list[str] genome: a list of strings representing a gene,
+                                 must be of length GENE_LENGTH, defaults to None
+        """
+        if genome:
+            assert(len(genome) == GENE_LENGTH)
+            self.gene = genome
+        else:
+            self.gene = self.generate_genome()
+        self.fitness = 0
+
+    def generate_genome(self) -> list[int]:
+        """generates a gene sequence
+
+        :return list[int]: a list of each individual gene
+        """
         gene = []
-
-        for _ in range(50):
+        for _ in range(GENE_LENGTH):
             r = random.randrange(4)
             gene.append(str(GENES[r]))
         
-        generated.append(gene)
+        return gene
     
-    return generated
+    def mutate(self):
+        """mutates each gene with a 2% chance (has a chance to mutate into itself, so true mutation rate is 1.5%)
+        """
+        for i in range(GENE_LENGTH):
+            if random.random() < .02:
+                self.gene[i] = GENES[random.randint(0,3)]
+
+def mate(parent1: Gene, parent2: Gene) -> Gene:
+    """generate offspring from parent1 and parent 2
+
+    :param Gene parent1: the first parent to derive the genome from
+    :param Gene parent2: the second parent to derive the genome from
+    """
+
+    # break points in gene -> 1-4; 5-20;                   (4bit)
+    #                         21-23; 24-26; 27-38; 39-50   (3bit)             
+
+
+def generate_genes(num_genes) -> list[Gene]:
+        """generates a list of gene sequences
+
+        :return list[gene]: a list of randomly generated genes
+        """
+        gene_list = []
+        for _ in range(num_genes):
+            gene = Gene()
+            gene_list.append(gene)
+        
+        return gene_list
 
 # simulates round robin competition between genes in list
 # returns percentage of wins for each placement
-def round_robin(genes: list[int]) -> list[float]:
-    results = [0] * len(genes)
+def round_robin(genes: list[Gene]):
+    """makes a round robin competition between every genome in genes
+
+    :param list[Gene] genes: list of genomes to test
+    :return None:
+    """
     for i in range(len(genes) - 1):
         for j in range(i+1, len(genes)):
-            (rounds, c1, c2) = _PyPacwar.battle(genes[i], genes[j])
+            (rounds, c1, c2) = _PyPacwar.battle(genes[i].gene, genes[j].gene)
             score = score_simulation(rounds, c1, c2)
             if score > 0:
-                results[i] += score
-                results[j] += 1 - score
+                genes[i].fitness += score
+                genes[j].fitness += 1 - score
             else:
-                results[i] += 1 + score
-                results[j] += -1 * score
+                genes[i].fitness += 1 + score
+                genes[j].fitness += -1 * score
     num_rounds = (len(genes) - 1) * len(genes) / 2
-    results = [n/num_rounds for n in results]
-    return results
+    for gene in genes:
+        gene.fitness /= num_rounds
+    return
 
-# scores both populations in a simulation proportional to 1, if c2 is the winner, the score will be negative
-# 10-10 --> .5, 7-13 --> -13/20, 20-0 --> 1
 def score_simulation(rounds, c1, c2) -> float:
+    """scores the results of a PyPacwar battle
+
+    :param _type_ rounds: number of rounds in the simulate
+    :param _type_ c1: ending population of the c1 genome
+    :param _type_ c2: ending population of the c2 genome
+    :return float: return scoring (proportional to 1) based on the PacWar specifications, 
+                   it will be negative if c2 wins examples: 10-10 --> .5, 7-13 --> -13/20, 20-0 --> 1
+    """
     score = 0 # higher score is better
     # checks if c1 or c2 wins
     c1_wins = 1 if c1 >= c2 else -1
@@ -77,9 +135,7 @@ def score_simulation(rounds, c1, c2) -> float:
 # Example Python module in C for Pacwar
 # https://www.geeksforgeeks.org/genetic-algorithms/
 def main():
-
-    num_genes = 50
-    gene_list = generate_genes(num_genes)
+    gene_list = generate_genes(POPULATION_SIZE)
     ones = [1] * 50
     threes = [3] * 50
     print("Example Python module in C for Pacwar")
@@ -92,8 +148,9 @@ def main():
 def test():
     # check generate_genes
     genes = generate_genes(4)
+    # print(genes)
     assert(len(genes) == 4)
-    assert([len(gene) for gene in genes] == [50] * 4)
+    assert([len(genome.gene) for genome in genes] == [50] * 4)
 
     # check score_simulation
     assert(score_simulation(10, 0, 100) == -1)
@@ -108,9 +165,9 @@ def test():
     assert(score_simulation(500, 100, 100) == 10/20)
 
     # check round robin
-    results = round_robin(genes)
+    round_robin(genes)
     # print(results)
-    assert(sum(results) == 1)
+    assert(sum([gene.fitness for gene in genes]) == 1)
 
 
 if __name__ == "__main__":
